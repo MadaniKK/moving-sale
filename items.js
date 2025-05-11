9db880ccc21ffd77b3daced816f1e61c14f2f6bb
Riley Li <qiwen_li@brown.edu>
2025-05-11 19:48:55 -0400

    add detail pages

--- items.js
+++ items.js
@@ -1,38 +1,48 @@
 // items.js
 export const items = [
-    {
-      title: 'Duracell AA Battery 24pcs',
-      description: 'Brand new. Unopened. Duracell AA Battery 24pcs with Power Boost ingredients',
-      category: 'electronics',
-      price: 10,
-      status: 'available',
-      images: [
-        // 'images/batteries_AA_24pcs.png',
-        'images/placeholder2.jpg',
-      ]
-    },
-    {
-      title: 'Office Chair',
-      description: 'Ergonomic swivel chair with lumbar support.',
-      category: 'furniture',
-      price: 10,
-      status: 'sold',
-      images: [
-        'images/placeholder.jpg',
-        'images/placeholder2.jpg',
-      ]
-    },
-    {
-      title: 'Floor Lamp',
-      description: 'Tall lamp with LED bulb included.',
-      category: 'lighting',
-      price: 0,
-      status: 'available',
-      images: [
-        'images/placeholder.jpg',
-        'images/placeholder2.jpg',
-      ]
-    },
-    // …add more items here, each with its own images array
-  ];
-  
+  {
+    title: "Duracell AA Battery 24pcs",
+    description:
+      "Brand new. Unopened. Duracell AA Battery 24pcs with Power Boost ingredients",
+    category: "electronics",
+    price: 0,
+    status: "available",
+    images: ["images/batteries_24pcs_1.png", "images/batteries_24pcs_2.png"],
+  },
+  {
+    title: "Collapsible Heated Foot Spa Tub",
+    description:
+      "Used Collapsible Foot Spa Bath with Heat,Collapsible Foot Spa Bath with Heat, Touch Screen, Temperature Control, Remote",
+    category: "electronics",
+    price: 0,
+    status: "available",
+    images: ["images/foottub_1.jpg", "images/foottub_2.png"],
+  },
+  {
+    title: "Floor Lamp",
+    description: "Tall lamp with LED bulb included.",
+    category: "lighting",
+    price: 0,
+    status: "available",
+    images: ["images/placeholder.jpg", "images/placeholder2.jpg"],
+  },
+  {
+    title: "Electric Kettle",
+    description:
+      "Used Speed-Boil Electric Kettle For Coffee & Tea - 1.7L Water Boiler 1500W, Borosilicate Glass, Easy Clean Wide Opening, Auto Shut-Off, Cool Touch Handle, LED Light. 360° Rotation, Boil Dry Protection",
+    category: "Kitchen",
+    price: 0,
+    status: "available",
+    images: ["images/kettle.png"],
+  },
+  {
+    title: "Kirkland Signature 13 Gallon Garbage Bags",
+    description:
+      "Kirkland Signature 13 Gallon 200 Ct Carton 100% recyclable Heavy Duty Drawstring Kitchen Trash Bags Garbage Bag,White, (85% left)",
+    category: "kitchen",
+    price: 0,
+    status: "available",
+    images: ["images/trashbags_13gallon.jpg"],
+  },
+  // …add more items here, each with its own images array
+];
