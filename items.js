135694e080649c6a0ac5fb8b4142b26b8896235a
Riley Li <qiwen_li@brown.edu>
2025-05-14 01:55:55 -0400

    add items

--- items.js
+++ items.js
@@ -29,7 +29,7 @@ export const items = [
   {
     title: "Electric Kettle",
     description:
-      "Used Speed-Boil Electric Kettle For Coffee & Tea - 1.7L Water Boiler 1500W, Borosilicate Glass, Easy Clean Wide Opening, Auto Shut-Off, Cool Touch Handle, LED Light. 360° Rotation, Boil Dry Protection",
+      "Used and cleaned Speed-Boil Electric Kettle For Coffee & Tea - 1.7L Water Boiler 1500W, Borosilicate Glass, Easy Clean Wide Opening, Auto Shut-Off, Cool Touch Handle, LED Light. 360° Rotation, Boil Dry Protection",
     category: "Kitchen",
     price: 0,
     status: "available",
@@ -38,11 +38,50 @@ export const items = [
   {
     title: "Kirkland Signature 13 Gallon Garbage Bags",
     description:
-      "Kirkland Signature 13 Gallon 200 Ct Carton 100% recyclable Heavy Duty Drawstring Kitchen Trash Bags Garbage Bag,White, (85% left)",
+      "Kirkland Signature 13 Gallon 200 Ct Carton 100% recyclable Heavy Duty Drawstring Kitchen Trash Bags Garbage Bag,White, (85% left) Swiffer Sweeper Wet Mopping Cloths with Febreze Freshness - Lavender Vanilla",
     category: "kitchen",
     price: 0,
     status: "available",
     images: ["images/trashbags_13gallon.jpg"],
   },
+  {
+    title: "Swiffer Wet Mopping Cloths - Lavendar",
+    description:
+      "Unopened Swiffer Sweeper Wet Mopping Cloths with Febreze Freshness - Lavender Vanilla 24pc",
+    category: "kitchen",
+    price: 0,
+    status: "available",
+    images: ["images/SwifferSweeperWetMoppingCloths.avif"],
+  },
   // …add more items here, each with its own images array
+  //Sterilite 66qt ClearView Latch Box Clear with Purple Latches: Plastic Storage Container, Lidded & Stackable, 66 Quart
+  {
+    title: "Six Lidded Plastic Storage Bins - 66 Quart",
+    description:
+      'Sterilite 66qt ClearView Latch Box Clear with Purple Latches: Plastic Storage Container, Lidded & Stackable, 66 Quart. Outside Dimensions: 23 5/8" x 16 3/8" x 13 1/4"',
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/storagebin.avif"],
+  },
+  {
+    title: "Cordless Vacuum Cleaner",
+    description:
+      "Cordless Vacuum Cleaner, 550W/45Kpa/65Mins Vacuum Cleaners for Home, Self-Standing Stick Vacuum with LED Display, Rechargeable Cordless Vacuum, Lightweight for Carpet, Pet Hair, Hardwood Floor",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/vacummcleaner.jpg"],
+  },
+  {
+    title: "4 Tier Plastic Stackable  Storage Bins ",
+    description:
+      "4 Tier Plastic Storage Bins and Boxes Organizer - Stackable with Lids for Bedroom, Living Room, Kitchen, Office - Foldable Containers. Total 93 Qt Capacity Multipurpose】Each storage box measures 15.35in*11in*8.27in (LxWxH). ",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/stackabledrawers.jpg"],
+  },
+
+  //Cordless Vacuum Cleaner, 550W/45Kpa/65Mins Vacuum Cleaners for Home, Self-Standing Stick Vacuum with LED Display, Rechargeable Cordless Vacuum, Lightweight for Carpet, Pet Hair, Hardwood Floor
 ];
