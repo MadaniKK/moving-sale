bdd63e208d5e2981becb1d9096ed44b8c943baac
Riley Li <qiwen_li@brown.edu>
2025-05-14 02:57:17 -0400

    add items and links

--- items.js
+++ items.js
@@ -12,7 +12,7 @@ export const items = [
   {
     title: "Collapsible Heated Foot Spa Tub",
     description:
-      "Used Collapsible Foot Spa Bath with Heat,Collapsible Foot Spa Bath with Heat, Touch Screen, Temperature Control, Remote",
+      "Collapsible Foot Spa Bath with Heat,Collapsible Foot Spa Bath with Heat, Touch Screen, Temperature Control, Remote",
     category: "electronics",
     price: 0,
     status: "available",
@@ -29,8 +29,8 @@ export const items = [
   {
     title: "Electric Kettle",
     description:
-      "Used and cleaned Speed-Boil Electric Kettle For Coffee & Tea - 1.7L Water Boiler 1500W, Borosilicate Glass, Easy Clean Wide Opening, Auto Shut-Off, Cool Touch Handle, LED Light. 360° Rotation, Boil Dry Protection",
-    category: "Kitchen",
+      "Speed-Boil Electric Kettle For Coffee & Tea - 1.7L Water Boiler 1500W, Borosilicate Glass, Easy Clean Wide Opening, Auto Shut-Off, Cool Touch Handle, LED Light. 360° Rotation, Boil Dry Protection",
+    category: "kitchen",
     price: 0,
     status: "available",
     images: ["images/kettle.png"],
@@ -72,16 +72,75 @@ export const items = [
     price: 0,
     status: "available",
     images: ["images/vacummcleaner.jpg"],
+    amazonUrl:
+      "https://www.amazon.com/Cordless-Cleaners-Self-standing-Rechargeable-Lightweight/dp/B0D8VSF1K8/ref=sr_1_5?crid=300TQHKZESVE4&dib=eyJ2IjoiMSJ9.W3MhRDhRuvcDr85w2zz0-DvLgaqdBtGnDyFgGFc9xxxnOJdPrgy4MBvvuiCXxuvhno6ia2IIF96RIWvKw5m012PP_WcZwpkN8aUEL6x6r_jv33x0SDxoQGu4Eo-KcqeotVtHV3E_ogK6uhCabLk3FryfsN40S4-anCAcCZ4kx-bTdMIq57itpElS1on1AGrSVwWSaO4O4CCnAtNUFCZGMxEjU9yw5DP1ZsNdnPuq9hxqrUobhGMnWTeupniHJWR8YFevdIr9EoiraT-e585Cq3qaHhkZfjhpHQX5bxxmHAo.ZWsevYJwwiNToVY4SBXeHQwtPX_xStAUWx-lfauaywc&dib_tag=se&keywords=hompany+cordless+vacuum+cleaner&qid=1747204062&s=home-garden&sprefix=hompany%2Cgarden%2C85&sr=1-5",
   },
   {
-    title: "4 Tier Plastic Stackable  Storage Bins ",
+    title: "4 Tier Plastic Stackable  Storage Bins",
     description:
       "4 Tier Plastic Storage Bins and Boxes Organizer - Stackable with Lids for Bedroom, Living Room, Kitchen, Office - Foldable Containers. Total 93 Qt Capacity Multipurpose】Each storage box measures 15.35in*11in*8.27in (LxWxH). ",
     category: "storage",
     price: 0,
     status: "available",
     images: ["images/stackabledrawers.jpg"],
+    amazonUrl:
+      "https://www.amazon.com/Plastic-Stackable-Containers-Collapsible-Organizers/dp/B0CHRP96MS/ref=sr_1_6?crid=W2E0FKF7J0E9&dib=eyJ2IjoiMSJ9.gW3xQfr8KXXLq6hm5ZfQRSJlLFzhMVqud3ny6Y_D6LPBOEO5xt1zLOHtLxUUBaCFZzgxil2Ec_j4LVsV4JJV4PuIBSQjHBZ56uU9BB1xsTGdm-cJ9nGeottPMNR4qz8rHLHQah3YAxL1EJVJZ7jxg7x1a5ACIFyp4iDIsd0qBOPYtE6MiF3PKmxgLS3q0OCkYKLJ0U64ipA-4b-N79hFa1AEqoy7X1NYCZcpzIGodOTtGYsL2DufqtkKYPGAJONpUTwjIagSYw32wAk42bQ6NLGmMNYcj189c0sVnpbqvZQ.YMRFZ7jNpu0pGZeGCEN9LYWskqd_I86nP4xmkzDHwjg&dib_tag=se&keywords=stackable%2Bdrawers&qid=1747203870&sprefix=stackable%2Bd%2Caps%2C97&sr=8-6&th=1",
   },
-
-  //Cordless Vacuum Cleaner, 550W/45Kpa/65Mins Vacuum Cleaners for Home, Self-Standing Stick Vacuum with LED Display, Rechargeable Cordless Vacuum, Lightweight for Carpet, Pet Hair, Hardwood Floor
+  {
+    title: "Dreo 6L Humidifiers for Bedroom, Smart",
+    description:
+      "Dreo 6L Humidifiers for Bedroom, Smart Warm & Cool Mist for Large Room, 60Hr Quiet Runtime for Baby, Plant, Humidity Sensor, Indicator Light, 12H Timer, Google/Alexa, HM713S",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/dreohumidifier_2.jpg", "images/dreohumidifier_1.jpg"],
+    amazonUrl:
+      "https://www.amazon.com/Dreo-Humidifier-Precision-Humidity-Indicator/dp/B0CB4CK3ZS/ref=sr_1_8?crid=FKR2M4YZFQH9&dib=eyJ2IjoiMSJ9.du5Hzz1UOg07z5f_R3YUiWEO5F0rj0xZWAZerSzIbHHNHqcjB27oY6u5JyWEPDhnI9bIt8LCoIdfvEbcp2HGBHkUAQLhTTWNABGxWVyBz4esq0CCaPukETZiHhcxtwreQJGwa_6mpyoySkz02h-JHqKa6WPJy0RaYiFt2I_BAG44JaPeDlA_1MY02D1n-KZbeIQbCiw3xWVneLipGaHOir4w6l_FkRDgHKACxYmvD_bBrv58p05ksSGTbzLdwG2y4jqd8R90avc9YHU0utwIxJAymoz5ubgrsy5tTsBePAw.5FQt-5cn_7NJR05TFRw-ZMo3ctlAaaW2PE-v5XruYX4&dib_tag=se&keywords=dreo%2Bhumidifier&qid=1747203658&sprefix=dreo%2Bhum%2Caps%2C115&sr=8-8&th=1",
+  },
+  {
+    title: "2 * 3 Tier Plastic Stackable Storage Bins",
+    description:
+      "6 Tier Plastic Storage Bins and Boxes Organizer - Stackable and collapsible with Lids for Bedroom, Living Room, Kitchen, Office - Foldable Containers. Each storage box measures 10.6″ D × 10.6″ H × 17.7″ W. ",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/stackableclothesdrawers.jpg"],
+    amazonUrl:
+      "https://www.temu.com/goods.html?refer_share_id=morO4bCRXeCmGTViFDM89jt7f7coO9Vt&refer_share_channel=copy_link&_bg_fs=1&share_video=https%3A%2F%2Fgoods-vod.kwcdn.com%2Fgoods-video%2F162e71192a5af851122270a65a09a4c131dba6d5.f30.mp4&from_share=1&share_imessage_opt=1&share_video_height=960&share_img=https%3A%2F%2Fimg.kwcdn.com%2Fproduct%2Ffancy%2F46d70674-0b42-4d65-88b5-f4aeccb5b427.jpg&goods_id=601099565202881&share_video_width=720&_oak_page_source=417&_oak_region=211&refer_share_suin=RSSBACMCAFIXU2I7HZQGA37EVRAFAUSCLI5IUGCUBB2G4RIQKGP36YH3LPNEIK7AVK3THWWGPQ&_x_share_id=morO4bCRXeCmGTViFDM89jt7f7coO9Vt&refer_page_name=goods&refer_page_id=10032_1747204731537_89rs8fwo6d&refer_page_sn=10032&_x_sessn_id=jc1fcgtv4r",
+  },
+  {
+    title:
+      "Tide Pods HE Compatible Laundry Detergent Pacs - Free & Gentle 42ct",
+    description:
+      "Unopened Tide Pods HE Compatible Laundry Detergent Pacs - Free & Gentle 42ct",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/laundrypods.jpg"],
+    amazonUrl:
+      "https://www.target.com/p/tide-pods-laundry-detergent-pacs-free-gentle/-/A-79394296?preselect=50225561#lnk=sametab",
+  },
+  {
+    title: "A lot of Plastic Hangers Room Essentials™",
+    description:
+      "Used 50pk Plastic Hangers White & Green & Black - Room Essentials™: Clothes Hangers for Shirts, Dresses, Tank Tops, Adult Closets",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/hangers.jpg"],
+    amazonUrl:
+      "https://www.target.com/p/tide-pods-laundry-detergent-pacs-free-gentle/-/A-79394296?preselect=50225561#lnk=sametab",
+  },
+  {
+    title:
+      "KIRKLAND SIGNATURE Stretch Tite Plastic Wrap Pack X 750' 1\" (1500 Sq') Count 1",
+    description:
+      "Unopened KIRKLAND SIGNATURE Stretch Tite Plastic Wrap Pack X 750' 1\" (1500 Sq') Count 1",
+    category: "storage",
+    price: 0,
+    status: "available",
+    images: ["images/plasticwrap.jpg"],
+    amazonUrl:
+      "https://www.amazon.com/Kirkland-Signature-Stretch-Tite-Plastic-Count/dp/B006B7QJ6K/ref=asc_df_B006B7QJ6K?mcid=b3950ec7695b3f6c938332d283f88874&hvocijid=4047205818463352992-B006B7QJ6K-&hvexpln=73&tag=hyprod-20&linkCode=df0&hvadid=721245378154&hvpos=&hvnetw=g&hvrand=4047205818463352992&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9002242&hvtargid=pla-2281435180258&psc=1",
+  },
 ];
